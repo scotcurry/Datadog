da83a85a9beb99ceb4cb5d07c4e65d19fc1d97b4
Scot Curry <scotcurry4@gmail.com>
2022-12-08 09:26:55 -0500

    Working for both Docker and k8s.  Need to work on logging still.

--- Containers/build_run_container.sh
+++ Containers/build_run_container.sh
@@ -1,4 +1,23 @@
+if [[ $# == '-l' ]]; then
+    echo "Pulling latest Datadog Agent"
+    docker rm --force datadog_agent
+    docker rmi --force datadog/agent:latest
+    docker pull datadog/agent:latest
+fi
+
 cd ..
-docker build --tag docker.io/scotcurry4/datadogcurryware:0.1.0 --file ./Docker/Dockerfile .
-cd Docker
+
+# Clean everything to start fresh every time.
+echo 'Removing Containers'
+docker rm --force datadogcurryware
+echo "Removing Images"
+docker rmi --force scotcurry4/datadogcurryware:0.2.0
+docker rmi --force docker.io/scotcurry4/datadogcurryware:0.2.0
+
+if [[ $1 == -u ]]; then
+  docker image push docker.io/scotcurry4/datadogcurryware:0.2.0
+fi
+
+docker build --tag docker.io/scotcurry4/datadogcurryware:0.2.0 --file ./Containers/Dockerfile .
+cd Containers
 docker-compose up -d
--- app.py
+++ app.py
@@ -1,15 +1,37 @@
 import datetime
+import logging
 
 from flask import Flask, request, render_template
-
+from ddtrace import tracer
+from ddtrace import patch; patch(logging=True)
 
 app = Flask(__name__)
 application = app
 app.secret_key = 'UdGspIJlMFSIeyaRTrrI'
 
+# This information will show up in a log search in the Datadog console
+FORMAT = ('%(asctime)s %(levelname)s [%(name)s] [%(filename)s:%(lineno)d] '
+          '[dd.service=%(dd.service)s dd.env=%(dd.env)s dd.version=%(dd.version)s dd.trace_id=%(dd.trace_id)s dd.span_id=%(dd.span_id)s] '
+          '- %(message)s')
+logging.basicConfig(format=FORMAT)
+logging.basicConfig(format=FORMAT)
+log = logging.getLogger(__name__)
+log.level = logging.INFO
 
+tracer.configure(
+    # This needs to match the container_name field in the docker-compose.yaml file for Docker
+    # TODO:  Need to add the information for k8s when I get it built.
+    hostname='datadog-agent',
+    port=8126
+)
+
+
+@tracer.wrap()
 @app.route('/', methods=['GET'])
 @app.route('/index', methods=['GET'])
 def index_page():
+    headers = request.headers
+    user_agent = headers.environ['HTTP_USER_AGENT']
     current_time = datetime.datetime.now()
+    log.info('Application starting with user-agent: %s', user_agent)
     return render_template('index.html', current_time=current_time)
