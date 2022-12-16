41a0539527a22e011b5c272cc19265415c51b313
Scot Curry <scotcurry4@gmail.com>
2022-12-15 20:15:42 -0500

    Updates to logging, Github Actions.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
+import os
 import datetime
 import logging
-import sys
 
 # from ddtrace import patch; patch(logging=True)
 from flask import Flask, request, render_template
@@ -10,8 +10,10 @@ app = Flask(__name__)
 application = app
 app.secret_key = 'UdGspIJlMFSIeyaRTrrI'
 
+running_local = os.environ['RUN_LOCAL']
+
 # This information will show up in a log search in the Datadog console
-if sys.gettrace() is None:
+if running_local is None:
     FORMAT = ('%(asctime)s %(levelname)s [%(name)s] [%(filename)s:%(lineno)d] '
               '[dd.service=%(dd.service)s dd.env=%(dd.env)s dd.version=%(dd.version)s dd.trace_id=%(dd.trace_id)s dd.span_id=%(dd.span_id)s] '
               '- %(message)s')
@@ -23,13 +25,6 @@ logging.basicConfig(format=FORMAT)
 log = logging.getLogger(__name__)
 log.level = logging.INFO
 
-# tracer.configure(
-#     # This needs to match the container_name field in the docker-compose.yaml file for Docker
-#     # TODO:  Need to add the information for k8s when I get it built.
-#     hostname='ddmac',
-#     port=8126
-# )
-
 
 @tracer.wrap()
 @app.route('/', methods=['GET'])
@@ -39,21 +34,7 @@ def index_page():
     # user_agent = headers.environ['HTTP_USER_AGENT']
     user_agent = headers.get('HTTP_USER_AGENT')
     current_time = datetime.datetime.now()
-    log.info('Application starting with user-agent: %s', user_agent)
+    log.d("Debug Messages - Don't show up in Indexes")
+    log.info('INFO message - Do show up in indexes')
 
-    log.info('Another log Info')
-    try:
-        span = tracer.current_span()
-        if span:
-            span_name = span.name
-            log.info('Span Name: ' + span_name)
-        else:
-            log.error('No span found')
-    except:
-        log.error('Exception in tracer.current_span()')
-
-    if span is None:
-        log.error('--- No span object ---')
-
-    correlation_ids = (span.trace_id, span.span_id) if span else (None, None)
     return render_template('index.html', current_time=current_time)
--- datadog_api/api_runner.py
+++ datadog_api/api_runner.py
@@ -2,16 +2,33 @@ import os
 
 from datadog_api_client import ApiClient, Configuration
 from datadog_api_client.v1.api.metrics_api import MetricsApi
+from datadog_api_client.v2.api.logs_api import LogsApi
+from datadog_api_client.v2.api.logs_archives_api import LogsArchivesApi
+from datadog_api_client.v1.api.logs_indexes_api import LogsIndexesApi
 
 
 dd_api_key = os.environ['DD_API_KEY']
 dd_app_key = os.environ['DD_APP_KEY']
 configuration = Configuration()
 with ApiClient(configuration) as api_client:
-    # api_instance = AuthenticationApi(api_client)
     api_instance = MetricsApi(api_client)
     response = api_instance.list_metrics(
         q="q",
     )
+    print('--- Available Metrics ---')
+    print(response)
 
+    logs_api_instance = LogsApi(api_client)
+    log_items = logs_api_instance.list_logs_get_with_pagination(page_limit=2)
+    for item in log_items:
+        print(item)
+
+    api_log_archive_instance = LogsArchivesApi(api_client)
+    response = api_log_archive_instance.list_logs_archives()
+    print('--- Archives are Storage Buckets that ARE NOT on Datadog Storage ---')
+    print(response)
+
+    log_indexes_api_instance = LogsIndexesApi(api_client)
+    response = log_indexes_api_instance.list_log_indexes()
+    print('--- Indexes are stored, the key concept here is exclusions ----')
     print(response)
