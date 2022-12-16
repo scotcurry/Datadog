b8f00a4c95b1630aee77fb2f7c6c1119807e1bc6
Scot Curry <scotcurry4@gmail.com>
2022-12-16 09:33:17 -0500

    Tweek to Github Action to try to get container to work on Mac.

--- Containers/build_run_container.sh
+++ Containers/build_run_container.sh
@@ -11,14 +11,14 @@ cd ..
 echo 'Removing Containers'
 docker rm --force datadogcurryware
 echo "Removing Images"
-docker rmi --force scotcurry4/datadogcurryware:0.2.1
-docker rmi --force docker.io/scotcurry4/datadogcurryware:0.2.1
+docker rmi --force scotcurry4/datadogcurryware:0.2.2
+docker rmi --force docker.io/scotcurry4/datadogcurryware:0.2.2
 
-docker build --tag docker.io/scotcurry4/datadogcurryware:0.2.1 --file ./Containers/Dockerfile .
-docker build --tag scotcurry4/datadogcurryware:0.2.1 --file ./Containers/Dockerfile .
+docker build --tag docker.io/scotcurry4/datadogcurryware:0.2.2 --file ./Containers/Dockerfile .
+docker build --tag scotcurry4/datadogcurryware:0.2.2 --file ./Containers/Dockerfile .
 
 if [[ $1 == -u ]]; then
-  docker image push docker.io/scotcurry4/datadogcurryware:0.2.1
+  docker image push docker.io/scotcurry4/datadogcurryware:0.2.2
 fi
 
 cd Containers
