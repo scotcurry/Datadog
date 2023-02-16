c33cd30e7a429a7a6a45d6c54ee458f446e3d053
Scot Curry <scotcurry4@gmail.com>
2023-02-16 15:15:14 -0500

    Validating that CI Pipeline works.  This should have no failed tests

--- unittests/datadog_rest_api_tests.py
+++ unittests/datadog_rest_api_tests.py
@@ -1,7 +1,7 @@
 from classes.datadog_rest_api import validate_api_keys, get_usage_by_product_family
 def test_validate_tests_work():
     variable = 'Otis'
-    assert 'o' in variable
+    assert 'O' in variable
 
 def test_validate_api_key():
     api_return = validate_api_keys()
